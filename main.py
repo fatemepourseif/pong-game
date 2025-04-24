ebf51065a12b961dd2b05307f7bfb36d5f154864
Fatemeh Pourseifollah Noshahr <fatemehpourseifollahnoshahr@MacBook-Pro-di-Fatemeh.local>
2025-04-24 09:08:04 +0200

    💾 Initial commit: Pong game files added

---
+++ main.py
@@ -0,0 +1,43 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.listen()
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_positions()
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_positions()
+
+    ball.move()
+
+screen.exitonclick()
